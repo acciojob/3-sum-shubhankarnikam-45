2cdaa8da75df875c7ea0c02b30af5cf4ddbc7504
shubhankarnikam-45 <120236922+shubhankarnikam-45@users.noreply.github.com>
2023-01-13 15:03:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function threeSum(arr, target) {
-  //your code here
+	let sum;
+	let num;
+
+	for(let i=0;i<arr.length-2;i++)
+		{
+			sum=0;
+			for(let j=i;j<=i+2;j++)
+				{
+					sum=sum+arr[j];
+				}
+			if(sum!=target && sum>=target+1)
+			{
+				num=sum;
+			}
+			return num;
+		}
+
 }
 
 module.exports = threeSum;
